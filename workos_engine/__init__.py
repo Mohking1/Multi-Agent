054c7c8c317b1582eb664cefc8a7cd17c310d7f7
Mohking1 <lokhandwalamohammed100@gmail.com>
2026-09-01 13:20:09 +0530

    Remove legacy prototype scripts and finalize WorkOS architecture

--- workos_engine/__init__.py
+++ workos_engine/__init__.py
@@ -1,7 +1,3 @@
 """WorkOS: Autonomous Executive AI Operating System."""
 
 __version__ = "1.0.0"
-
-from workos_engine.planner import ExecutivePlanner
-
-__all__ = ["ExecutivePlanner", "__version__"]
